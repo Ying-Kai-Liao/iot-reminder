0287fd87af36f9af5d4e4e56ebb6d2785b9e62f7
Ying-Kai Liao <yklworkingaddress@gmail.com>
2023-06-04 09:52:50 +0800

    try

--- pages/api/line/webhook.ts
+++ pages/api/line/webhook.ts
@@ -50,7 +50,7 @@ const textEventHandler = async (
         }
         
         // Send the data to register api
-        await axios.post('/api/user/register', data)
+        await axios.post('https//:localhost:443/api/user/register', data)
           .then(res => reply = JSON.stringify(res.data))
           .catch(err => reply = JSON.stringify(err.data))
         
